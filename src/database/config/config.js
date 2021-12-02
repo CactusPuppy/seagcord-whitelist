02db707365da2467613e5ece89397f815dacd26b
CactusPuppy <puppycactus@gmail.com>
2021-12-01 22:51:29 -0800

    build: Fix Heroku Postgresql DB connection

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -12,8 +12,8 @@ module.exports = {
   production: {
     url: process.env.DATABASE_URL,
     dialect: "postgres",
-    ssl: {
-      rejectUnauthorized: false,
-    }
+    dialectOptions: {
+      ssl: true,
+    },
   },
 };
